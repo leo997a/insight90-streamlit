fa700b2b9590416987c96552d5d163b07cef03c0
leo997a <166352340+leo997a@users.noreply.github.com>
2025-04-23 03:47:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ match_url = st.text_input("أدخل رابط المباراة من WhoScored:")
 if st.button("تحليل"):
     with st.spinner("جارٍ استخراج البيانات..."):
         try:
-            api_url = f"https://insight90-api.onrender.com/extract?url={match_url}"  # غيّر الرابط حسب ما ظهر لك من Render
+            api_url = f"https://insight90-backend.vercel.app/extract?url={match_url}"  # غيّر الرابط حسب ما ظهر لك من Render
             response = requests.get(api_url, timeout=30)
             data = response.json()
             if "error" in data:
